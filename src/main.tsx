1693e0ef1b8850f3dd0717e75a27b126e3f4f5a8
vmdvdv-npt <vmdvdv@gmail.com>
2026-08-21 11:46:53 +0400

    Load visual panel overrides

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import './styles.css';
+import './visual-overrides.css';
 import './renderer2d/stone-placement.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
