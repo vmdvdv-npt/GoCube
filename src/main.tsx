c497ba8371706f375a749981149d60315cb9be00
vmdvdv-npt <vmdvdv@gmail.com>
2026-08-21 18:04:10 +0400

    feat: expose isolated Cube 2D preview route

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
+import { Cube2DPreview } from './app/Cube2DPreview';
 import './styles.css';
 import './visual-overrides.css';
 import './renderer2d/stone-placement.css';
 
+const showCube2DPreview = new URLSearchParams(window.location.search).has('cube2d-preview');
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    {showCube2DPreview ? <Cube2DPreview /> : <App />}
   </React.StrictMode>,
 );
