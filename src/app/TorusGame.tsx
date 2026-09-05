0ff1caa5653062de6df3b035dec26fd7e6c4f39f
vmdvdv-npt <vmdvdv@gmail.com>
2026-09-05 23:38:01 +0400

    Bind Torus UI to its final analysis progress source

--- src/app/TorusGame.tsx
+++ src/app/TorusGame.tsx
@@ -1,1 +1,15 @@
-export { TorusGame, type TorusGameProps } from './TorusGameBase';
+import { FinalAnalysisProgressProvider } from './FinalAnalysisProgressContext';
+import {
+  TorusGame as TorusGameBase,
+  type TorusGameProps,
+} from './TorusGameBase';
+
+export type { TorusGameProps };
+
+export function TorusGame(props: TorusGameProps) {
+  return (
+    <FinalAnalysisProgressProvider source={props.controller.finalAnalysisProgressSource()}>
+      <TorusGameBase {...props} />
+    </FinalAnalysisProgressProvider>
+  );
+}
