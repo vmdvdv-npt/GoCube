efa4fe107e7328102b64125e2855af9131a42c9f
vmdvdv-npt <vmdvdv@gmail.com>
2026-09-08 02:45:06 +0400

    fix: stop view-owned Torus disposal

--- src/app/TorusGame.tsx
+++ src/app/TorusGame.tsx
@@ -1,15 +1,9 @@
 import { FinalAnalysisProgressProvider } from './FinalAnalysisProgressContext';
-import {
-  TorusGame as TorusGameBase,
-  type TorusGameProps,
-} from './TorusGameBase';
-import { useGameControllerLifecycle } from './useGameControllerLifecycle';
-
-export type { TorusGameProps };
+import { TorusGameBase, type TorusGameProps } from './TorusGameBase';
+import './manual-endgame.css';
+import './game-viewport.css';
 
 export function TorusGame(props: TorusGameProps) {
-  useGameControllerLifecycle(props.controller);
-
   return (
     <FinalAnalysisProgressProvider source={props.controller.finalAnalysisProgressSource()}>
       <TorusGameBase {...props} />
