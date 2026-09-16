820c498450c65e4db96acf04856c6e8bd6f77294
vmdvdv-npt <vmdvdv@gmail.com>
2026-09-16 19:28:44 +0400

    fix: remove obsolete endgame floating control mount

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './app/App';
 import { Cube2DPreview } from './app/Cube2DPreview';
-import { EndgameGroupFloatingControls } from './app/EndgameGroupFloatingControls';
 import { installTorus2DVectorCameraGuard } from './renderer2d/Torus2DVectorCameraGuard';
 import './styles.css';
 import './visual-overrides.css';
@@ -14,14 +13,5 @@ const showCube2DPreview = new URLSearchParams(window.location.search).has('cube2
 installTorus2DVectorCameraGuard();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    {showCube2DPreview ? (
-      <Cube2DPreview />
-    ) : (
-      <>
-        <App />
-        <EndgameGroupFloatingControls />
-      </>
-    )}
-  </React.StrictMode>,
+  <React.StrictMode>{showCube2DPreview ? <Cube2DPreview /> : <App />}</React.StrictMode>,
 );
